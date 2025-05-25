ef5e082bcb19a402522c9feea5caf7f4f3f17489
Marwan <maroelsays2013@gmail.com>
2025-05-25 19:14:49 +0300

    Huffman enhanced

--- src/HuffmanCompresser.java
+++ src/HuffmanCompresser.java
@@ -12,7 +12,7 @@ n:window Size   int
 fileSize int
 compFileLastLen  // for 0's and 1's  byte
 endofFilelen    byte
-MapLength       int
+MapSize       int
 CompressedMap
 File
 endOfFile  // trimmed from inputed file to make file length divisble by n
@@ -20,8 +20,8 @@ endOfFile  // trimmed from inputed file to make file length divisble by n
 */
 public class HuffmanCompresser {
 
-    private HashMap<String,String> sequenceCode;
-    private String endOfFile = "";    //Thrown at the end of the file
+    private HashMap<ByteArrayWrapper,String> sequenceCode;
+    private ByteArrayWrapper endOfFile = new ByteArrayWrapper(new byte[0]);    // Thrown at the end of the file
     private byte endOfFileLen = 0;
     private int fileSize = 0;
     private byte compFileLastLen;
@@ -36,33 +36,29 @@ public class HuffmanCompresser {
         return Files.readAllBytes(filePath);
     }
 
-    public HashMap<String,Integer> getFrequency(byte[] fileBytes,int n){
-
+    public HashMap<ByteArrayWrapper, Integer> getFrequency(byte[] fileBytes, int n) {
         System.out.println("Original file size = " + fileBytes.length + " Bytes");
         fileSize = fileBytes.length;
-        if(fileBytes.length%n != 0){
-            endOfFile = new String(fileBytes,fileBytes.length-fileBytes.length%n,fileBytes.length%n);
-            endOfFileLen = (byte)endOfFile.length();
+        if (fileBytes.length % n != 0) {
+            endOfFile = new ByteArrayWrapper(Arrays.copyOfRange(fileBytes, fileBytes.length - fileBytes.length % n, fileBytes.length));
+            endOfFileLen = (byte) endOfFile.getContents().length;
         }
-
-        HashMap<String,Integer> map = new HashMap<>();
-        int sz = fileBytes.length - fileBytes.length%n;
-        for(int i=0;i<sz;i+=n){
-            String resultString = new String(fileBytes, i, n);
-            Integer currentValue = map.get(resultString);
-            if (currentValue != null) map.put(resultString, currentValue + 1);
-            else map.put(resultString, 1);
+        HashMap<ByteArrayWrapper, Integer> map = new HashMap<>();
+        int sz = fileBytes.length - fileBytes.length % n;
+        for (int i = 0; i < sz; i += n) {
+            byte[] segment = Arrays.copyOfRange(fileBytes, i, i + n);
+            ByteArrayWrapper key = new ByteArrayWrapper(segment);
+            map.put(key, map.getOrDefault(key, 0) + 1);
         }
-
         return map;
     }
 
-    public HuffmanNode createHuffmanTree(HashMap<String,Integer> freqMap){
+    public HuffmanNode createHuffmanTree(HashMap<ByteArrayWrapper,Integer> freqMap){
 
         Comparator<HuffmanNode> frequencyComparator = Comparator.comparingInt(node -> node.freq);
 
         PriorityQueue<HuffmanNode> priorityQueue = new PriorityQueue<>(frequencyComparator);
-        for (Map.Entry<String, Integer> entry : freqMap.entrySet()) {
+        for (Map.Entry<ByteArrayWrapper, Integer> entry : freqMap.entrySet()) {
             HuffmanNode node = new HuffmanNode(null,null,entry.getKey(),entry.getValue());
             priorityQueue.add(node);
         }
@@ -85,34 +81,37 @@ public class HuffmanCompresser {
         generateCode(node.right,val+"1");
     }
 
-    public byte[] compressMap(){
-        List<Byte> compressedMapByte = new ArrayList<>();
-        for (Map.Entry<String, String> entry : this.sequenceCode.entrySet()) {
-
-            // Put the key first
-            byte[] keyBytes = entry.getKey().getBytes();
-            for(int i=0;i<keyBytes.length;i++)
-                compressedMapByte.add(keyBytes[i]);
-
-            //encode the value then put it to the file
-            String val = entry.getValue();
-            for(int i=0;i<val.length();i+=7){
-                String binaryString = val.substring(i,min(i+7,val.length()));
-                byte b = (byte) Integer.parseInt(binaryString, 2);
-                compressedMapByte.add(b);
+    public byte[] compressMap() {
+        ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+        DataOutputStream dos = new DataOutputStream(byteStream);
+        try {
+            for (Map.Entry<ByteArrayWrapper, String> entry : sequenceCode.entrySet()) {
+                byte[] keyBytes = entry.getKey().getContents();
+                String code = entry.getValue();
+                // 1. Write key length (1 byte) + key bytes
+                dos.writeByte(keyBytes.length);  // Key length (0-255)
+                dos.write(keyBytes);             // Actual key bytes
+                // 2. Write code length in bits (2 bytes) + packed bits
+                int bitLength = code.length();
+                dos.writeShort(bitLength);
+                // Pack the Huffman code into bytes
+                int pos = 0;
+                while (pos < bitLength) {
+                    byte b = 0;
+                    int bitsToPack = Math.min(8, bitLength - pos);
+                    for (int i = 0; i < bitsToPack; i++) {
+                        if (code.charAt(pos + i) == '1') {
+                            b |= (byte) (1 << (7 - i));  // Set the bit
+                        }
+                    }
+                    dos.writeByte(b);
+                    pos += 8;
+                }
             }
-            byte len = (byte) (val.length()%7); //length of the last byte
-            compressedMapByte.add(len);
-
-            //Add a special charcter to diff
-            compressedMapByte.add((byte)150);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to compress Huffman map", e);
         }
-
-        byte[] mapBytesToFile = new byte[compressedMapByte.size()];
-        int i = 0;
-        for(byte b:compressedMapByte) mapBytesToFile[i++] = b;
-
-        return mapBytesToFile;
+        return byteStream.toByteArray();
     }
 
     public byte[] compressFile(byte[] fileBytes,int n){
@@ -120,12 +119,13 @@ public class HuffmanCompresser {
         StringBuilder st = new StringBuilder();
         int sz = fileBytes.length - fileBytes.length%n;
         for(int i=0;i<sz;i+=n){
-            String s = new String(fileBytes, i, n);
+            ByteArrayWrapper s = new ByteArrayWrapper(Arrays.copyOfRange(fileBytes, i, i + n));
             st.append(sequenceCode.get(s));
         }
 
         int len = st.length(),j=0;
         compFileLastLen = (byte) (st.length()%8);
+        System.out.println("compFileLastLen:"+ compFileLastLen);
         byte[] bytesToFile = new byte[(int) ceil(st.length()/8.0)];
         for(int i=0;i<st.length();i+=8){
             String binaryString = st.substring(i,min(i+8,st.length()));
@@ -151,7 +151,6 @@ public class HuffmanCompresser {
         double compressionRatio = (double) compressedFileSize/fileSize;
         System.out.println("Compression ratio = " + compressionRatio);
 
-
 //        System.out.println("Map Before Saving");
 //        for(int i=0;i<compressedMap.length;i++)
 //            System.out.print((char)compressedMap[i]);
@@ -172,7 +171,7 @@ public class HuffmanCompresser {
         dos.writeInt(compressedMap.length);
         bufferedOutputStream.write(compressedMap);
         bufferedOutputStream.write(bytesToFile);
-        bufferedOutputStream.write(endOfFile.getBytes());
+        bufferedOutputStream.write(endOfFile.getContents());
 
         bufferedOutputStream.close();
 
@@ -211,13 +210,15 @@ public class HuffmanCompresser {
         byte[] fileBytes = readNormalFile(inputFilePath);
 
         // Get Frequency Map
-        HashMap<String,Integer> map = getFrequency(fileBytes,n);
-
+        HashMap<ByteArrayWrapper,Integer> map = getFrequency(fileBytes,n);
         //Create Huffman Tree
         HuffmanNode root = createHuffmanTree(map);
 
         //Generate code for each n bytes
-        generateCode(root,"");
+        generateCode(root, "");
+//        for (Map.Entry<ByteArrayWrapper, String> entry : sequenceCode.entrySet()) {
+//            System.out.println(Arrays.toString(entry.getKey().getContents()) + " : " + entry.getValue());
+//        }
 
         //Return Compressed File
         byte[] bytesToFile = compressFile(fileBytes,n);
--- src/HuffmanNode.java
+++ src/HuffmanNode.java
@@ -1,17 +1,22 @@
 public class HuffmanNode {
     HuffmanNode left, right;
-    String ch;
+    ByteArrayWrapper ch;
     int freq;
-    HuffmanNode(){
+
+    HuffmanNode() {
         this.left = this.right = null;
-        ch = "";
-        freq=0;
+        ch = null;
+        freq = 0;
     }
-    HuffmanNode(HuffmanNode left, HuffmanNode right, String ch, int freq){
+
+    HuffmanNode(HuffmanNode left, HuffmanNode right, ByteArrayWrapper ch, int freq) {
         this.left = left;
         this.right = right;
         this.ch = ch;
         this.freq = freq;
     }
-    public boolean isLeaf(){return this.left==null && this.right ==null;}
+
+    public boolean isLeaf() {
+        return this.left == null && this.right == null;
+    }
 }
